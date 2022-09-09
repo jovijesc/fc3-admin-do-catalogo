d5907e4db0cb5f51f89078fc4b7d7d665d8c2e95
Jhonatas Vicente de Jesus <jovijesc@gmail.com>
2022-09-09 15:13:01 -0300

    Implementando caso de uso de criação de categoria

---
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/category/create/CreateCategoryUseCase.java
@@ -0,0 +1,9 @@
+package com.fullcycle.admin.catalogo.application.category.create;
+
+import com.fullcycle.admin.catalogo.application.UseCase;
+import com.fullcycle.admin.catalogo.domain.category.CategoryGateway;
+
+public abstract class CreateCategoryUseCase
+    extends UseCase<CreateCategoryCommand, CreateCategoryOutput> {
+
+}
