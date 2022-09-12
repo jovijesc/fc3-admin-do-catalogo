5b64d4739715092d1b4a499e94e6bc446f32915f
Jhonatas Vicente de Jesus <jovijesc@gmail.com>
2022-09-12 17:14:52 -0300

    Implementando o caso de uso de get de categoria por id

---
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/category/retrieve/get/GetCategoryByIdUseCase.java
@@ -0,0 +1,8 @@
+package com.fullcycle.admin.catalogo.application.category.retrieve.get;
+
+import com.fullcycle.admin.catalogo.application.UseCase;
+
+public abstract class GetCategoryByIdUseCase
+    extends UseCase<String, CategoryOutput> {
+
+}
