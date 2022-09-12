8d3390f2cf36b8464037fba6bd6f977298eba8df
Jhonatas Vicente de Jesus <jovijesc@gmail.com>
2022-09-12 11:31:54 -0300

    Implementando testes e caso de uso de atualização de categoria

---
+++ application/src/test/java/com/fullcycle/admin/catalogo/application/category/update/UpdateCategoryUseCaseTest.java
@@ -0,0 +1,80 @@
+package com.fullcycle.admin.catalogo.application.category.update;
+
+import com.fullcycle.admin.catalogo.domain.category.Category;
+import com.fullcycle.admin.catalogo.domain.category.CategoryGateway;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.AdditionalAnswers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.swing.text.html.Option;
+import java.util.Objects;
+import java.util.Optional;
+
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class UpdateCategoryUseCaseTest {
+
+    @InjectMocks
+    private DefaultUpdateCategoryUseCase useCase;
+
+    @Mock
+    private CategoryGateway categoryGateway;
+
+    // 1. Teste do caminho feliz
+    // 2. Teste passandouma propriedade inválida(name)
+    // 3. Teste atualizando uma categoria para inativa
+    // 4. Teste simulando um erro generico vindo do gateway
+    // 5. Teste atualizar categoria passando ID inválido
+
+    @Test
+    public void ginveAValidCommand_whenCallsUpdateCategory_shouldReturnCategoryId() {
+        final var aCategory =
+                Category.newCategory( "Film", null, true);
+
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var expectedId = aCategory.getId();
+
+        final var aCommand = UpdateCategoryCommand.with(
+                expectedId.getValue(),
+                expectedName,
+                expectedDescription,
+                expectedIsActive
+        );
+
+        when(categoryGateway.findById(eq(expectedId)))
+                .thenReturn(Optional.of(Category.with(aCategory)));
+
+        when(categoryGateway.update(any()))
+                .thenAnswer(returnsFirstArg());
+
+        final var actualOutput = useCase.execute(aCommand).get();
+
+        Assertions.assertNotNull(actualOutput);
+        Assertions.assertNotNull(actualOutput.id());
+
+        Mockito.verify(categoryGateway, Mockito.times(1)).findById(eq(expectedId));
+
+        Mockito.verify(categoryGateway, Mockito.times(1)).update(argThat(
+           aUpdateCategory ->
+                   Objects.equals(expectedName, aUpdateCategory.getName())
+                           && Objects.equals(expectedDescription, aUpdateCategory.getDescription())
+                           && Objects.equals(expectedIsActive, aUpdateCategory.isActive())
+                           && Objects.equals(expectedId, aUpdateCategory.getId())
+                           && Objects.equals(aCategory.getCreatedAt(), aUpdateCategory.getCreatedAt())
+                           && aCategory.getUpdatedAt().isBefore(aUpdateCategory.getUpdatedAt())
+                           && Objects.isNull(aUpdateCategory.getDeletedAt())
+        ));
+
+    }
+
+}
--- domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/Category.java
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/Category.java
@@ -2,11 +2,10 @@ package com.fullcycle.admin.catalogo.domain.category;
 
 import com.fullcycle.admin.catalogo.domain.AggregateRoot;
 import com.fullcycle.admin.catalogo.domain.validation.ValidationHandler;
-
 import java.time.Instant;
-import java.util.UUID;
 
-public class Category extends AggregateRoot<CategoryID> {
+
+public class Category extends AggregateRoot<CategoryID> implements Cloneable {
 
     private String name;
     private String description;
@@ -38,6 +37,18 @@ public class Category extends AggregateRoot<CategoryID> {
         final var now = Instant.now();
         final var deletedAt = isActive ? null : now;
         return new Category(id, aName, aDescription, isActive, now, now, deletedAt);
+    }
+
+    public static Category with(final Category aCategory) {
+        return new Category(
+                aCategory.getId(),
+                aCategory.name,
+                aCategory.description,
+                aCategory.isActive(),
+                aCategory.createdAt,
+                aCategory.updatedAt,
+                aCategory.deletedAt
+        );
     }
 
     @Override
@@ -104,5 +115,14 @@ public class Category extends AggregateRoot<CategoryID> {
 
     public Instant getDeletedAt() {
         return deletedAt;
+    }
+
+    @Override
+    protected Category clone()  {
+        try {
+            return (Category) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
     }
 }
