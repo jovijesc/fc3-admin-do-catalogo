20c481aade0dc5292e1ddda49071dd8a2949a92b
Jhonatas Vicente de Jesus <jovijesc@gmail.com>
2022-09-13 16:46:18 -0300

    Implementando jpa repository da categoria

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
@@ -1,15 +1,33 @@
 package com.fullcycle.admin.catalogo.infrastructure;
 
 import com.fullcycle.admin.catalogo.application.UseCase;
+import com.fullcycle.admin.catalogo.domain.category.Category;
+import com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity;
+import com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
 import com.fullcycle.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.AbstractEnvironment;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner runner(CategoryRepository repository) {
+        return args -> {
+            final List<CategoryJpaEntity> all = repository.findAll();
+            Category filme = Category.newCategory("Filmes", null, true);
+            repository.saveAndFlush(CategoryJpaEntity.from(filme));
+            repository.deleteAll();
+
+        };
     }
 }
