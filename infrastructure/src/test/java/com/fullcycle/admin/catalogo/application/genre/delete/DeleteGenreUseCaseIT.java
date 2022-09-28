6ca082ca162e2e80ba232c4d848f9983bc56ae4a
Jhonatas Vicente de Jesus <jovijesc@gmail.com>
2022-09-28 11:25:59 -0300

    Implementando testes integrados do caso de uso de delete genre

---
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/application/genre/delete/DeleteGenreUseCaseIT.java
@@ -0,0 +1,67 @@
+package com.fullcycle.admin.catalogo.application.genre.delete;
+
+import com.fullcycle.admin.catalogo.IntegrationTest;
+import com.fullcycle.admin.catalogo.domain.genre.Genre;
+import com.fullcycle.admin.catalogo.domain.genre.GenreGateway;
+import com.fullcycle.admin.catalogo.domain.genre.GenreID;
+import com.fullcycle.admin.catalogo.infrastructure.genre.persistence.GenreRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+
+@IntegrationTest
+public class DeleteGenreUseCaseIT {
+
+    @Autowired
+    private DeleteGenreUseCase useCase;
+
+    @Autowired
+    private GenreGateway genreGateway;
+
+    @Autowired
+    private GenreRepository genreRepository;
+
+    @Test
+    public void givenAValidGenreId_whenCallsDeleteGenre_shouldDeleteGenre() {
+        // given
+        final var aGenre = genreGateway.create(Genre.newGenre("Ação", true));
+
+        final var expectedId = aGenre.getId();
+
+        Assertions.assertEquals(1, genreRepository.count());
+
+        // when
+        Assertions.assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
+
+        // then
+        Assertions.assertEquals(0, genreRepository.count());
+
+    }
+
+    @Test
+    public void givenAnInvalidGenreId_whenCallsDeleteGenre_shouldBeOk() {
+        // given
+        final var aGenre = genreGateway.create(Genre.newGenre("Ação", true));
+
+        final var expectedId = GenreID.from("123");
+
+        Assertions.assertEquals(1, genreRepository.count());
+
+        // when
+        Assertions.assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
+
+        // then
+        Assertions.assertEquals(1, genreRepository.count());
+
+    }
+
+}
