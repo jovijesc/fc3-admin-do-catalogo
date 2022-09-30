924ea77a0f7e531e87ebcd392c65fdb3d76ba306
Jhonatas Vicente de Jesus <jovijesc@gmail.com>
2022-09-30 11:37:02 -0300

    Implementando o TDD da operação de update castmember

--- domain/src/test/java/com/fullcycle/admin/catalogo/domain/castmember/CastMemberTest.java
+++ domain/src/test/java/com/fullcycle/admin/catalogo/domain/castmember/CastMemberTest.java
@@ -94,4 +94,123 @@ public class CastMemberTest {
         Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
     }
 
+    @Test
+    public void ginveAValidCastMember_whenCallUpdate_shouldReceiveUpdated() {
+        final var expectedName = " ";
+        final var expectedType = CastMemberType.ACTOR;
+
+        final var actualMember = CastMember.newMember("vind", CastMemberType.DIRECTOR);
+
+        Assertions.assertNotNull(actualMember);
+        Assertions.assertNotNull(actualMember.getId());
+
+        final var actualID = actualMember.getId());
+        final var actualCreatedAt = actualMember.getCreatedAt();
+        final var actualUpdatedAt = actualMember.getUpdatedAt();
+
+        actualMember.update(expectedName, expectedType);
+
+        Assertions.assertEquals(actualID, actualMember.getId());
+        Assertions.assertEquals(expectedName, actualMember.getName());
+        Assertions.assertEquals(expectedType, actualMember.getType());
+        Assertions.assertEquals(actualCreatedAt, actualMember.getCreatedAt());
+        Assertions.assertTrue(actualUpdatedAt.isBefore(actualMember.getUpdatedAt()));
+    }
+
+    @Test
+    public void ginveAValidCastMember_whenCallUpdateWithInvalidNullName_shouldReceiveNotification() {
+        final String expectedName = null;
+        final var expectedType = CastMemberType.ACTOR;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var actualMember = CastMember.newMember("vind", CastMemberType.DIRECTOR);
+
+        Assertions.assertNotNull(actualMember);
+        Assertions.assertNotNull(actualMember.getId());
+
+
+        final var actualException = Assertions.assertThrows(NotificationException.class,
+                () -> actualMember.update(expectedName, expectedType)
+        );
+
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void ginveAValidCastMember_whenCallUpdateWithInvalidEmptyName_shouldReceiveNotification() {
+        final var expectedName = " ";
+        final var expectedType = CastMemberType.ACTOR;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be empty";
+
+        final var actualMember = CastMember.newMember("vind", CastMemberType.DIRECTOR);
+
+        Assertions.assertNotNull(actualMember);
+        Assertions.assertNotNull(actualMember.getId());
+
+
+        final var actualException = Assertions.assertThrows(NotificationException.class,
+                () -> actualMember.update(expectedName, expectedType)
+        );
+
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void ginveAValidCastMember_whenCallUpdateWithLengthMoreThan255_shouldReceiveANotification() {
+        final String expectedName = """
+                O que temos que ter sempre em mente é que o desafiador cenário globalizado prepara-nos para enfrentar situações atípicas decorrentes 
+                dos conhecimentos estratégicos para atingir a excelência. Todavia, a determinação clara de objetivos cumpre um papel essencial na formulação do 
+                remanejamento dos quadros funcionais. Desta maneira, o fenômeno da Internet causa impacto indireto na reavaliação dos relacionamentos verticais 
+                entre as hierarquias. O empenho em analisar o surgimento do comércio virtual garante a contribuição de um grupo importante na determinação das direções 
+                preferenciais no sentido do progresso. A certificação de metodologias que nos auxiliam a lidar com a estrutura atual da organização exige a precisão e a 
+                definição das novas proposições.
+                """;
+
+        final var expectedType = CastMemberType.ACTOR;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' must be between 3 and 255 characters";
+
+        final var actualMember = CastMember.newMember("vind", CastMemberType.DIRECTOR);
+
+        Assertions.assertNotNull(actualMember);
+        Assertions.assertNotNull(actualMember.getId());
+
+
+        final var actualException = Assertions.assertThrows(NotificationException.class,
+                () -> actualMember.update(expectedName, expectedType)
+        );
+
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void ginveAValidCastMember_whenCallUpdateWithInvalidNullType_shouldReceiveNotification() {
+        final var expectedName = "Vin Diesel";
+        final CastMemberType expectedType = null;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'type' should not be null";
+
+        final var actualMember = CastMember.newMember("vind", CastMemberType.DIRECTOR);
+
+        Assertions.assertNotNull(actualMember);
+        Assertions.assertNotNull(actualMember.getId());
+
+
+        final var actualException = Assertions.assertThrows(NotificationException.class,
+                () -> actualMember.update(expectedName, expectedType)
+        );
+
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
 }
