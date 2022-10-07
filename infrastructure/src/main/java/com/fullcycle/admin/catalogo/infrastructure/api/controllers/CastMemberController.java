7b11d80ed051c962f358936909a4e3a385115e65
Jhonatas Vicente de Jesus <jovijesc@gmail.com>
2022-10-07 09:03:29 -0300

    Implementando o endpoint de get castmember por id

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/controllers/CastMemberController.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/controllers/CastMemberController.java
@@ -2,8 +2,11 @@ package com.fullcycle.admin.catalogo.infrastructure.api.controllers;
 
 import com.fullcycle.admin.catalogo.application.castmember.create.CreateCastMemberCommand;
 import com.fullcycle.admin.catalogo.application.castmember.create.CreateCastMemberUseCase;
+import com.fullcycle.admin.catalogo.application.castmember.retrieve.get.GetCastMemberByIdUseCase;
 import com.fullcycle.admin.catalogo.infrastructure.api.CastMemberAPI;
+import com.fullcycle.admin.catalogo.infrastructure.castmember.models.CastMemberResponse;
 import com.fullcycle.admin.catalogo.infrastructure.castmember.models.CreateCastMemberRequest;
+import com.fullcycle.admin.catalogo.infrastructure.castmember.presenter.CastMemberPresenter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +18,13 @@ public class CastMemberController implements CastMemberAPI {
 
 
     private final CreateCastMemberUseCase createCastMemberUseCase;
+    private final GetCastMemberByIdUseCase getCastMemberByIdUseCase;
 
-    public CastMemberController(final CreateCastMemberUseCase createCastMemberUseCase) {
+    public CastMemberController(
+            final CreateCastMemberUseCase createCastMemberUseCase,
+            final GetCastMemberByIdUseCase getCastMemberByIdUseCase) {
         this.createCastMemberUseCase = Objects.requireNonNull(createCastMemberUseCase);
+        this.getCastMemberByIdUseCase = Objects.requireNonNull(getCastMemberByIdUseCase);
     }
 
     @Override
@@ -27,5 +34,10 @@ public class CastMemberController implements CastMemberAPI {
         final var output = this.createCastMemberUseCase.execute(aCommand);
 
         return ResponseEntity.created(URI.create("/cast_members/" + output.id())).body(output);
+    }
+
+    @Override
+    public CastMemberResponse getById(final String id) {
+        return CastMemberPresenter.present(this.getCastMemberByIdUseCase.execute(id));
     }
 }
