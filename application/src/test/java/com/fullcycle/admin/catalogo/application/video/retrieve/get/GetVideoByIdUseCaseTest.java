cab2224b9659172c1c7c25ef1a5a0b6f667d8d20
Jhonatas Vicente de Jesus <jovijesc@gmail.com>
2022-10-17 11:00:23 -0300

    Implementando testes unitários do caso de uso get video por id

---
+++ application/src/test/java/com/fullcycle/admin/catalogo/application/video/retrieve/get/GetVideoByIdUseCaseTest.java
@@ -0,0 +1,140 @@
+package com.fullcycle.admin.catalogo.application.video.retrieve.get;
+
+import com.fullcycle.admin.catalogo.application.Fixture;
+import com.fullcycle.admin.catalogo.application.UseCaseTest;
+import com.fullcycle.admin.catalogo.domain.exceptions.NotFoundException;
+import com.fullcycle.admin.catalogo.domain.video.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.time.Year;
+import java.util.*;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+public class GetVideoByIdUseCaseTest extends UseCaseTest  {
+
+    @InjectMocks
+    private DefaultGetVideoByIdUseCase useCase;
+
+    @Mock
+    private VideoGateway videoGateway;
+
+    @Override
+    protected List<Object> getMocks() {
+        return List.of(videoGateway);
+    }
+
+    @Test
+    public void givenAValidId_whenCallsGetVideo_shouldReturnIt() {
+        // given
+        final var expectedTitle = Fixture.title();
+        final var expectedDescription = Fixture.Videos.description();
+        final var expectedLauncYear = Year.of(Fixture.year());
+        final var expectedDuration = Fixture.duration();
+        final var expectedOpened = Fixture.bool();
+        final var expectedPublished = Fixture.bool();
+        final var expectedRating = Fixture.Videos.rating();
+        final var expectedCategories = Set.of(Fixture.Categories.aulas().getId());
+        final var expectedGenres = Set.of(Fixture.Genres.tech().getId());
+        final var expectedMembers = Set.of(
+                Fixture.CastMembers.wesley().getId(),
+                Fixture.CastMembers.paulo().getId()
+        );
+        final var expectedVideo = audioVideo(Resource.Type.VIDEO);
+        final var expectedTrailer = audioVideo(Resource.Type.TRAILER);
+        final var expectedBanner = image(Resource.Type.BANNER);
+        final var expectedThumb = image(Resource.Type.THUMBNAIL);
+        final var expectedThumbHalf = image(Resource.Type.THUMBNAIL_HALF);
+
+        final var aVideo = Video.newVideo(
+                expectedTitle,
+                expectedDescription,
+                expectedLauncYear,
+                expectedDuration,
+                expectedOpened,
+                expectedPublished,
+                expectedRating,
+                expectedCategories,
+                expectedGenres,
+                expectedMembers
+        )
+                .setVideo(expectedVideo)
+                .setTrailer(expectedTrailer)
+                .setBanner(expectedBanner)
+                .setThumbnail(expectedThumb)
+                .setThumbnailHalf(expectedThumbHalf);
+
+        final var expectedId = aVideo.getId();
+
+        when(videoGateway.findById(any()))
+                .thenReturn(Optional.of(Video.with(aVideo)));
+
+        // when
+        final var actualVideo = this.useCase.execute(expectedId.getValue());
+
+        // then
+        Assertions.assertEquals(expectedId, actualVideo.id());
+        Assertions.assertEquals(expectedTitle, actualVideo.title());
+        Assertions.assertEquals(expectedDescription, actualVideo.description());
+        Assertions.assertEquals(expectedLauncYear, actualVideo.launchedAt());
+        Assertions.assertEquals(expectedDuration, actualVideo.duration());
+        Assertions.assertEquals(expectedOpened, actualVideo.opened());
+        Assertions.assertEquals(expectedPublished, actualVideo.published());
+        Assertions.assertEquals(expectedRating, actualVideo.rating());
+        Assertions.assertEquals(expectedCategories, actualVideo.categories());
+        Assertions.assertEquals(expectedGenres, actualVideo.genres());
+        Assertions.assertEquals(expectedMembers, actualVideo.castMembers());
+        Assertions.assertEquals(expectedVideo, actualVideo.video());
+        Assertions.assertEquals(expectedTrailer, actualVideo.trailer());
+        Assertions.assertEquals(expectedBanner, actualVideo.banner());
+        Assertions.assertEquals(expectedThumb, actualVideo.thumbnail());
+        Assertions.assertEquals(expectedThumbHalf, actualVideo.thumbnailHalf());
+        Assertions.assertEquals(aVideo.getCreatedAt(), actualVideo.createdAt());
+        Assertions.assertEquals(aVideo.getUpdatedAt(), actualVideo.updatedAt());
+    }
+
+    @Test
+    public void givenInvalidId_whenCallsGetVideo_shouldReturnNotFound() {
+        // given
+        final var expectedErrorMessage = "Video with ID 123 not found";
+
+        final var expectedId = VideoID.from("123");
+
+        when(videoGateway.findById(any()))
+                .thenReturn(Optional.empty());
+
+        // when
+        final var actualException = Assertions.assertThrows(NotFoundException.class,
+                () -> this.useCase.execute(expectedId.getValue()));
+
+        // then
+        Assertions.assertEquals(expectedErrorMessage, actualException.getMessage());
+    }
+
+
+    private AudioVideoMedia audioVideo(final Resource.Type type) {
+        final var checksum = UUID.randomUUID().toString();
+        return AudioVideoMedia.with(
+                checksum,
+                type.name().toLowerCase(),
+                "/videos/" + checksum,
+                "",
+                MediaStatus.PENDING
+        );
+    }
+
+    private ImageMedia image(final Resource.Type type) {
+        final var checksum = UUID.randomUUID().toString();
+        return ImageMedia.with(
+                checksum,
+                type.name().toLowerCase(),
+                "/images/" + checksum
+        );
+    }
+
+}
