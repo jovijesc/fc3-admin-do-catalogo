16934b08762b22d7d52912db3d0d95fce2ca82ad
Jhonatas Vicente de Jesus <jovijesc@gmail.com>
2023-03-10 14:57:55 -0300

    Adicionando configuracao de beans e qualificadores para injecao do spring

---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/configuration/AmqpConfig.java
@@ -0,0 +1,27 @@
+package com.fullcycle.admin.catalogo.infrastructure.configuration;
+
+import com.fullcycle.admin.catalogo.infrastructure.configuration.annotations.VideoCreatedQueue;
+import com.fullcycle.admin.catalogo.infrastructure.configuration.annotations.VideoEncodedQueue;
+import com.fullcycle.admin.catalogo.infrastructure.configuration.properties.amqp.QueueProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmqpConfig {
+
+    @Bean
+    @ConfigurationProperties("amqp.queues.video-created")
+    @VideoCreatedQueue
+    public QueueProperties videoCreatedQueueProperties() {
+        return new QueueProperties();
+    }
+
+    @Bean
+    @ConfigurationProperties("amqp.queues.video-encoded")
+    @VideoEncodedQueue
+    public QueueProperties videoEncodedQueueProperties() {
+        return new QueueProperties();
+    }
+
+}
