0abd1e3b80ea90add69aab49859fa3f7fc79e0b1
Jhonatas Vicente de Jesus <jovijesc@gmail.com>
2023-05-04 10:01:43 -0300

    Criando estrutura de sandbox com docker

---
+++ sandbox/run.sh
@@ -0,0 +1,8 @@
+# Criando pastas com permissoes
+mkdir -m 777 .docker
+mkdir -m 777 .docker/keycloak
+
+docker compose -f app/docker-compose.yml up -d
+
+echo "Inicializando os containers ..."
+sleep 20
